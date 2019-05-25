0af94af1204d435220e971b6ad6d32901082adf0
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-05-25 12:53:03 -0400

    using the .join() method I was able to display an array of underscores without commas

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -54,8 +54,14 @@ let randomFruit = fruits[Math.floor(Math.random() * fruits.length)]
 console.log(randomFruit.name);
 
 let fruit = randomFruit.name;
-console.log(fruit)
+
 let fruitPhoto = randomFruit.photo;
+
+// Grabs the id's of the HTML elements and saves it to a variable
+let wordToGuess = document.getElementById("word-to-guess");
+let guessedLetters = document.getElementById("letters-guessed");
+let winsText = document.getElementById("wins");
+let lossestext = document.getElementById("losses");
 
 
 // User input
@@ -68,5 +74,12 @@ document.onkeyup = (event) => {
 
         wordGuess[i] = "_";
     }
-    console.log(wordGuess)
+
+    // Dynmaically displays the HTML, the wordGuess.join() method displays the underscores without commas   
+    wordToGuess.innerHTML = "Guess the fruit: " + wordGuess.join(" ");
+    guessedLetters.innerHTML = "Letters guessed: " + lettersGuessed;
+    winsText.innerHTML = "Wins: " + wins;
+
+    console.log(wordGuess.join(" "));
+    
 }
